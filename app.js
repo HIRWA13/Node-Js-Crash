39e9b1ead9eb289012873774c49cda0b947485ce
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-01 19:01:13 +0200

    feat: __filename, __dirname

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const sayInfo = require('./sayInfo')
 console.log(sayInfo) 
 sayInfo.sayName(sayInfo.username)
 sayInfo.sayAddress(sayInfo.username)
+
+console.log(`directory name: ${__dirname}`)
+console.log(`filename: ${__filename}`)

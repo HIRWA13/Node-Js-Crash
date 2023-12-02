b7d6bda02fc49badb44f005aa8b31b0d52c99fe7
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-02 14:18:43 +0200

    feat: write files with fs node module

---
+++ fs.js
@@ -0,0 +1,16 @@
+// fs module or file system module
+// it helps us to work with files on our computer
+
+const fs = require('fs');
+
+
+//syntax:  fs.writeFile (filename, value or data, callbackFn)
+
+const data = "hello world, i have a nice job and I will be happy with your business"
+
+fs.writeFile("message.txt", data, ((err) => {
+    if(err) throw err;
+    console.log('file has been written')
+}));
+
+console.log("let's see if it is asynchronous or not")

90695547c5dea9ed6ac8ba60d66cbe7ad328f8e7
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-02 17:06:23 +0200

    feat: reading an html file

--- httpUrl.js
+++ httpUrl.js
@@ -1,14 +1,17 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 // we can use the url parameter that comes with the request object and then handle what's called server side routing.
 
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write("<h1>Hello user, welcome to the homepage!</h1>")
-        res.write("<h2>Do you like Kambouchaa???</h2>")
-        res.write("<h4>I know you do, don't lie to us</h4>")
-        res.end() // ending the request
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) =>{
+          if (err) throw err;
+          res.writeHead(200, { "Content-Type": "text/html" });
+           res.write(data);
+          res.end(); // ending the request
+        })
     }
     if(req.url === '/user') {
         res.write("Hi Junior, this page is for users!")
